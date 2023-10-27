8538f94a758e15ad8ff3b8a4a81d5015ccc197bd
skyeler yearsley <s2krighty@gmail.com>
2023-10-27 12:02:19 -0500

    Completetion of Review Model

---
+++ FullStackAuth_WebAPI/Models/Review.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Review
+    {
+        {
+            [Key]
+            public int Id { get; set; }
+
+            [Required]
+            public string BookId { get; set; }
+
+            [Required]
+            public string Text { get; set; }
+
+            [Required]
+            public double Rating { get; set; }
+
+            [ForeignKey("Owner")]
+            public string UserId { get; set; }
+
+            public User User { get; set; }
+        }
+    }
+}
