4c583f61dbc080b5f1663eccc7534f9ddf429069
skyeler yearsley <s2krighty@gmail.com>
2023-10-27 12:17:44 -0500

    Complettion of BookDetailDto

--- FullStackAuth_WebAPI/DataTransferObjects/BookDetailsDto.cs
+++ FullStackAuth_WebAPI/DataTransferObjects/BookDetailsDto.cs
@@ -2,5 +2,10 @@
 {
     public class BookDetailsDto
     {
+        public int Id { get; set; }
+        public string BookId { get; set; }
+        public string Title { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public UserForDisplayDto User { get; set; }
     }
 }
